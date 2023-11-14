f569995a57287e50efb9bb551dab81b2aa63ccc6
Vladislav Pochernin <vspochernin@gmail.com>
2023-11-14 20:21:18 +0300

    18-initialize spring boot application

---
+++ server/src/test/java/ru/spbstu/tesseract/TesseractApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spbstu.tesseract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesseractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
