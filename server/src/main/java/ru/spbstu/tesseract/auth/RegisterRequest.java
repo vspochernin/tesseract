ab16a02a0aa55c07b8a8f9fb8217696b25e5f523
Vladislav <vspochernin@gmail.com>
2023-11-24 23:12:26 +0300

    ready to implement authentication controller

---
+++ server/src/main/java/ru/spbstu/tesseract/auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package ru.spbstu.tesseract.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String login;
+    private String email;
+    private String password;
+}
