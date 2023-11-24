2df014e97835f7808175a395055ce60cdd598d06
Vladislav <vspochernin@gmail.com>
2023-11-24 23:36:53 +0300

    implement authenticate

--- server/src/main/java/ru/spbstu/tesseract/auth/AuthenticationService.java
+++ server/src/main/java/ru/spbstu/tesseract/auth/AuthenticationService.java
@@ -1,6 +1,8 @@
 package ru.spbstu.tesseract.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.spbstu.tesseract.config.JwtService;
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         User user = User.builder()
@@ -31,7 +34,20 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getLogin(),
+                        request.getPassword()
+                )
+        );
+
+        User user = repository.findByLogin(request.getLogin())
+                .orElseThrow();
+
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
 
