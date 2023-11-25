4e7ed1c0b5999f26a2d813e0eaba647a118f3517
Vladislav <vspochernin@gmail.com>
2023-11-25 23:14:08 +0300

    change endpoints paths

--- server/src/main/java/ru/spbstu/tesseract/auth/AuthenticationController.java
+++ server/src/main/java/ru/spbstu/tesseract/auth/AuthenticationController.java
@@ -4,24 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService service;
 
-    @PostMapping("/register")
+    @PostMapping("/users")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("/login/tesseract")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
