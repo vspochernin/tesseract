74e099f62e6e57453adf5122ec006b921d4be63d
Vladislav Razukrantov <vrazbusiness@mail.ru>
2023-11-28 10:30:44 +0300

    change types datetime, add additional restrictions pk, not null

--- server/src/main/resources/db-scripts/tesseract_db_create.sql
+++ server/src/main/resources/db-scripts/tesseract_db_create.sql
@@ -4,19 +4,19 @@
 CREATE TABLE companies(
     id SERIAL PRIMARY KEY,  -- автоувеличение
     title VARCHAR(100) NOT NULL,  -- проанализировал - сейчас максимум 80, но возьмем с запасом
-    description VARCHAR(400),  -- если мы тут полностью будем хранить всю краткую информацию
-    foundation_datetime TIMESTAMP,
-    revenue BIGINT,
-    profit BIGINT,
-    staff INT
+    description VARCHAR(400) NOT NULL,  -- если мы тут полностью будем хранить всю краткую информацию
+    foundation_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
+    revenue BIGINT NOT NULL,
+    profit BIGINT NOT NULL,
+    staff INTEGER NOT NULL
 );
 
 -- Создаем таблицу для продуктов активов
 CREATE TABLE assets(
     id SERIAL PRIMARY KEY,  -- автоувеличение
 	title VARCHAR(30) NOT NULL,  -- проанализировал - сейчас максимум 11, но возьмем с запасом
-	description VARCHAR(400),  -- если мы тут полностью будем хранить всю краткую информацию
-	release_datetime TIMESTAMP,
+	description VARCHAR(400) NOT NULL,  -- если мы тут полностью будем хранить всю краткую информацию
+	release_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
 	company_id INTEGER NOT NULL,
 	interest NUMERIC NOT NULL,  -- процент доходности
 	FOREIGN KEY (company_id) REFERENCES companies (id) ON DELETE CASCADE -- когда пропадает
@@ -28,8 +28,8 @@ CREATE TABLE assets(
 CREATE TABLE prices(
     id SERIAL PRIMARY KEY,  -- автоувеличение
     asset_id INTEGER NOT NULL,
-    price INTEGER NOT NULL,  -- не numeric??
-	set_datetime TIMESTAMP NOT NULL,
+    price INTEGER NOT NULL,
+	set_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
 	FOREIGN KEY (asset_id) REFERENCES assets (id) ON DELETE CASCADE
 );
 
@@ -44,26 +44,25 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,  -- автоувеличение
     login VARCHAR(16) NOT NULL,  -- максимальная длина логина
 	email VARCHAR(320) NOT NULL,  -- у нас в ФС не сказано, максимум возможен 320
-	password VARCHAR(130) NOT NULL -- длина sha-512
+	password VARCHAR(60) NOT NULL -- длина bcrypt
 );
 
 -- Создаем таблицу связи пользователей и активов
 CREATE TABLE users_assets(
     user_id INTEGER NOT NULL,
 	asset_id INTEGER NOT NULL,
+	PRIMARY KEY(user_id, asset_id),
 	FOREIGN KEY (asset_id) REFERENCES assets (id) ON DELETE SET NULL,
-	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
-	-- можно и каскадное сделать
-	-- по-хорошему сюда еще PK нужен
+	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL	
 );
 
 -- Создаем таблицу диверсификаций
 CREATE TABLE diversifications(
     id SERIAL PRIMARY KEY,  -- автоувеличение
 	user_id INTEGER NOT NULL,
-	create_datetime TIMESTAMP,
+	create_datetime TIMESTAMP WITH TIME ZONE NOT NULL,
 	risk_id INTEGER NOT NULL,
-	amount INTEGER,  -- общая стоимость диверсификации я полагаю
+	amount INTEGER NOT NULL,  -- общая стоимость диверсификации я полагаю
 	FOREIGN KEY (risk_id) REFERENCES risks (id) ON DELETE SET NULL,
 	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
 	-- можно и каскадное сделать
@@ -73,7 +72,8 @@ CREATE TABLE diversifications(
 CREATE TABLE diversifications_assets(
     diversification_id INTEGER NOT NULL,
 	asset_id INTEGER NOT NULL,
-	count INTEGER,
+	PRIMARY KEY(diversification_id, asset_id),
+	count INTEGER NOT NULL,
 	FOREIGN KEY (diversification_id) REFERENCES diversifications (id) ON DELETE SET NULL,
 	FOREIGN KEY (asset_id) REFERENCES assets (id) ON DELETE SET NULL
 	-- можно и каскадное сделать
