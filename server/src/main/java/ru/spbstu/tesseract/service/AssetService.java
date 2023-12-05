8b727b66bc6bd394286f99fa8e9bde38f77bca1d
Vladislav Pochernin <vspochernin@gmail.com>
2023-12-05 17:42:48 +0300

    remove redundant method

--- server/src/main/java/ru/spbstu/tesseract/service/AssetService.java
+++ server/src/main/java/ru/spbstu/tesseract/service/AssetService.java
@@ -3,11 +3,9 @@ package ru.spbstu.tesseract.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.spbstu.tesseract.dto.AssetShortDto;
 import ru.spbstu.tesseract.entity.Asset;
-import ru.spbstu.tesseract.entity.User;
 import ru.spbstu.tesseract.repository.AssetRepository;
 
 import java.util.List;
@@ -22,16 +20,6 @@ public class AssetService {
         Slice<Asset> assets = assetRepository.findBy(PageRequest.of(pageNumber, pageSize));
 
         return assets.getContent().stream()
-                .map(AssetShortDto::fromAsset)
-                .toList();
-    }
-
-    public List<AssetShortDto> getFavouriteAssets(Integer pageNumber, Integer pageSize) {
-        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Slice<Asset> favouriteAssets = assetRepository
-                .findByUsersContains(currentUser, PageRequest.of(pageNumber, pageSize));
-
-        return favouriteAssets.getContent().stream()
                 .map(AssetShortDto::fromAsset)
                 .toList();
     }
