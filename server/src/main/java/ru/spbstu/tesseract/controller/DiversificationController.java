51dbdfce4f77be19b558a469a5ae8123c1af9a8c
Islam Murzakanov <murzakanovislam21@gmail.com>
2023-12-05 20:49:12 +0300

    implement endpoint

--- server/src/main/java/ru/spbstu/tesseract/controller/DiversificationController.java
+++ server/src/main/java/ru/spbstu/tesseract/controller/DiversificationController.java
@@ -1,16 +1,18 @@
 package ru.spbstu.tesseract.controller;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.spbstu.tesseract.dto.DiversificationShortDto;
 import ru.spbstu.tesseract.entity.Diversification;
 import ru.spbstu.tesseract.service.DiversificationService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,6 +20,20 @@ import java.util.List;
 public class DiversificationController {
 
     private final DiversificationService diversificationService;
+
+    @GetMapping("/diversifications")
+    public ResponseEntity<List<DiversificationShortDto>> diversifications(
+            @RequestParam(value = "pageNumber", defaultValue = "0") int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize
+    )
+    {
+        List<DiversificationShortDto> diversifications =
+                diversificationService.getDiversifications(pageNumber, pageSize);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(diversifications);
+    }
 
     @GetMapping("/testDiversifications")
     public ResponseEntity<List<Diversification>> getAllDiversifications() {
--- server/src/main/java/ru/spbstu/tesseract/entity/Diversification.java
+++ server/src/main/java/ru/spbstu/tesseract/entity/Diversification.java
@@ -1,14 +1,28 @@
 package ru.spbstu.tesseract.entity;
 
-import jakarta.persistence.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Entity
 @Data
@@ -37,7 +51,8 @@ public class Diversification {
             Date createDatetime,
             RiskType riskType,
             int amount,
-            DiversificationAsset... diversificationsAssets) {
+            DiversificationAsset... diversificationsAssets)
+    {
         this.user = user;
         this.createDatetime = createDatetime;
         this.riskType = riskType;
@@ -47,5 +62,11 @@ public class Diversification {
         }
         this.diversificationAssetSet = Stream.of(diversificationsAssets)
                 .collect(Collectors.toSet());
+    }
+
+    public String convertCreateDatetimeToString() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("MSK"));
+        return simpleDateFormat.format(createDatetime);
     }
 }
