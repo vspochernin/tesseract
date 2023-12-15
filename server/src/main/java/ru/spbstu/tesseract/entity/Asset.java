2066252ba8d4cde6af8fac54a1b472125211f388
Vladislav Razukrantov <vrazbusiness@mail.ru>
2023-12-15 21:00:32 +0300

    implement getting asset risk

--- server/src/main/java/ru/spbstu/tesseract/entity/Asset.java
+++ server/src/main/java/ru/spbstu/tesseract/entity/Asset.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.security.core.context.SecurityContextHolder;
+import java.time.Duration;
 
 import java.time.ZonedDateTime;
 import java.util.*;
@@ -55,10 +56,122 @@ public class Asset {
                 .anyMatch(user -> user.getLogin().equals(userLogin));
     }
 
-    public RiskType getRiskType() {
-        // TODO: mock, will be implemented in 48.
+    public long getAssetAge() {
+        ZonedDateTime currentDateTime = ZonedDateTime.now();
+        Duration duration = Duration.between(releaseDatetime, currentDateTime);
+        return duration.toDays();
+    }
 
-        return RiskType.getById(id % RiskType.values().length);
+    public int getScoreCompanyFoundation() {
+        double yearsUntilFoundation = getCompany().getCompanyAge();
+        if (yearsUntilFoundation < 1) {
+            return 1;
+        } else if (yearsUntilFoundation < 3) {
+            return 2;
+        } else if (yearsUntilFoundation < 6) {
+            return 3;
+        } else if (yearsUntilFoundation < 10) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public int getScoreCompanyRevenue() {
+        long companyRevenue = getCompany().getRevenue();
+        if (companyRevenue < 10000000000L) {
+            return 1;
+        } else if (companyRevenue < 50000000000L) {
+            return 2;
+        } else if (companyRevenue < 100000000000L) {
+            return 3;
+        } else if (companyRevenue < 500000000000L) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public int getScoreCompanyProfit() {
+        long companyProfit = getCompany().getProfit();
+        if (companyProfit < -1000000000L) {
+            return 1;
+        } else if (companyProfit < 0L) {
+            return 2;
+        } else if (companyProfit < 1000000000L) {
+            return 3;
+        } else if (companyProfit < 10000000000L) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public int getScoreCompanyStaffCount() {
+        int companyStaffCount = getCompany().getStaff();
+        if (companyStaffCount < 10) {
+            return 1;
+        } else if (companyStaffCount < 50) {
+            return 2;
+        } else if (companyStaffCount < 100) {
+            return 3;
+        } else if (companyStaffCount < 500) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public int getScoreAssetRelease() {
+        long daysUntilRelease = getAssetAge();
+        if (daysUntilRelease < 10) {
+            return 1;
+        } else if (daysUntilRelease < 30) {
+            return 2;
+        } else if (daysUntilRelease < 90) {
+            return 3;
+        } else if (daysUntilRelease < 180) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public int getScoreAssetInterest() {
+        if (interest > 17) {
+            return 1;
+        } else if (interest > 15) {
+            return 2;
+        } else if (interest > 13) {
+            return 3;
+        } else if (interest > 10) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+
+    public int getResultScore() {
+        int scoreCompanyFoundation = getScoreCompanyFoundation();
+        int scoreCompanyRevenue = getScoreCompanyRevenue();
+        int scoreCompanyProfit = getScoreCompanyProfit();
+        int scoreCompanyStaffCount = getScoreCompanyStaffCount();
+        int scoreAssetRelease = getScoreAssetRelease();
+        int scoreAssetInterest = getScoreAssetInterest();
+        return scoreCompanyFoundation + scoreCompanyRevenue + scoreCompanyProfit +
+                scoreCompanyStaffCount + scoreAssetRelease + scoreAssetInterest;
+    }
+
+    public RiskType getRiskType() {
+        int resultScore = getResultScore();
+        if (resultScore < 14) {
+            return RiskType.HIGH;
+        } else if (resultScore < 21) {
+            return RiskType.MIDDLE;
+        } else {
+            return RiskType.LOW;
+        }
+        //return RiskType.getById(id % RiskType.values().length);
     }
 
     public Optional<Integer> getOldPrice(ZonedDateTime atTheMoment) {
