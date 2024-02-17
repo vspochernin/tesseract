d52520d3492e9015be4aea2cac97864afcbeb9b8
Vladislav Pochernin <vspochernin@gmail.com>
2024-02-17 16:29:02 +0300

    100: add isValidPassword tests

---
+++ server/src/test/java/ru/spbstu/tesseract/utils/FieldValidatorTest.java
@@ -0,0 +1,75 @@
+package ru.spbstu.tesseract.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author pochernin-vla
+ */
+class FieldValidatorTest {
+
+    @Test
+    public void givenValidPassword_whenIsValidPassword_thenReturnTrue() {
+        String validPassword = "qwe123&*(";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(validPassword);
+
+        assertThat(actualIsValidPassword).isTrue();
+    }
+
+    @Test
+    public void givenTooShortPassword_whenIsValidPassword_thenReturnFalse() {
+        String tooShortPassword = "qw1&";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(tooShortPassword);
+
+        assertThat(actualIsValidPassword).isFalse();
+    }
+
+    @Test
+    public void givenTooLongPassword_whenIsValidPassword_thenReturnFalse() {
+        String tooLongPassword = "qwe123%^&qwe123%^&qwe123%^&qwe123%^&qwe123%^&qwe123%^&qwe123%^&";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(tooLongPassword);
+
+        assertThat(actualIsValidPassword).isFalse();
+    }
+
+    @Test
+    void givenPasswordWithNoLetters_whenIsValidPassword_thenReturnFalse() {
+        String passWordWithNoLetters = "123&*(";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(passWordWithNoLetters);
+
+        assertThat(actualIsValidPassword).isFalse();
+    }
+
+    @Test
+    void givenPasswordWithNoDigitsOrSpecialSymbols_whenIsValidPassword_thenReturnFalse() {
+        String passwordWithNoDigitsOrSpecialSymbols = "qweqweqwe";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(passwordWithNoDigitsOrSpecialSymbols);
+
+        assertThat(actualIsValidPassword).isFalse();
+    }
+
+    @Test
+    void givenPasswordWithUnknownSymbol_whenIsValidPassword_thenReturnFalse() {
+        String passwordWithUnknownSymbol = "qwe123&*(±";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(passwordWithUnknownSymbol);
+
+        assertThat(actualIsValidPassword).isFalse();
+    }
+
+    @Test
+    void givenEmptyPassword_whenIsValidPassword_thenReturnFalse() {
+        String emptyPassword = "";
+
+        boolean actualIsValidPassword = FieldValidator.isValidPassword(emptyPassword);
+
+        assertThat(actualIsValidPassword).isFalse();
+    }
+}
