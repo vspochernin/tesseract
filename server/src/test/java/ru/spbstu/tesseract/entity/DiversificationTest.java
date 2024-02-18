a22d28bfdcdfea908de103200ab652ff119c61ea
Vladislav Pochernin <vspochernin@gmail.com>
2024-02-18 10:50:33 +0300

    102: minor fixes

--- server/src/test/java/ru/spbstu/tesseract/entity/DiversificationTest.java
+++ server/src/test/java/ru/spbstu/tesseract/entity/DiversificationTest.java
@@ -1,13 +1,12 @@
 package ru.spbstu.tesseract.entity;
 
-import org.junit.jupiter.api.Test;
-
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Set;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class DiversificationTest {
     @Test
@@ -15,14 +14,15 @@ class DiversificationTest {
         Diversification diversification = Diversification.builder()
                 .diversificationAssetSet(Set.of(
                         DiversificationAsset.builder()
-                                .Asset.builder()
-                                .prices(List.of(
-                                                Price.builder()
-                                                        .price(100)
-                                                        .setDatetime(ZonedDateTime.now())
-                                                        .build()
+                                .asset(Asset.builder()
+                                        .prices(List.of(
+                                                        Price.builder()
+                                                                .price(100)
+                                                                .setDatetime(ZonedDateTime.now())
+                                                                .build()
+                                                )
                                         )
-                                )
+                                        .build())
                                 .count(10)
                                 .build()))
                 .build();
