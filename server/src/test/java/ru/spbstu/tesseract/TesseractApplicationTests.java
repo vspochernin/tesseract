17677afe44166440f6d91292cf96381172c5aa26
Vladislav Pochernin <vspochernin@gmail.com>
2024-02-25 15:43:42 +0300

    121: add test containers to context test

--- server/src/test/java/ru/spbstu/tesseract/TesseractApplicationTests.java
+++ server/src/test/java/ru/spbstu/tesseract/TesseractApplicationTests.java
@@ -2,9 +2,29 @@ package ru.spbstu.tesseract;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TesseractApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14.7")
+			.withDatabaseName("tesseract")
+			.withUsername("postgres")
+			.withPassword("postgres");
+
+	@DynamicPropertySource
+	static void postgresqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.password", postgres::getPassword);
+		registry.add("spring.datasource.username", postgres::getUsername);
+	}
+
 
 	@Test
 	void contextLoads() {
